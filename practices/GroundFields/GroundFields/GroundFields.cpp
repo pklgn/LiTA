6653321a0ac37d77f3dcf23c0f65be9987f5ec7b
pklgn <petechka_petrov_201@mail.ru>
2022-03-02 18:38:01 +0300

    added third practice task

---
+++ practices/GroundFields/GroundFields/GroundFields.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+
+int getMinAmount(int N)
+{
+    int minAmount = 5;
+    if (N == 0)
+    {
+        return 0;
+    }
+    for (int i = sqrt(N); i >= 1; i--)
+    {
+        int currAmount = getMinAmount(N - i * i) + 1;
+        if (currAmount == 1)
+        {
+            return currAmount;
+        }
+        if (currAmount < minAmount)
+        {
+            minAmount = currAmount;
+        }
+    }
+
+    return minAmount;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc == 1)
+    {
+        std::cout << "Please enter the file name" << std::endl;
+
+        return 1;
+    }
+    std::ifstream sourceFile(argv[1]);
+    int N;
+    sourceFile >> N;
+    int result = getMinAmount(N);
+    std::cout << result << std::endl;
+}
