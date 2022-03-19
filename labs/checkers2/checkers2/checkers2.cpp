a64d5fad6c1a27354e2d2dece51515115542fc70
pklgn <petechka_petrov_201@mail.ru>
2022-03-20 00:23:45 +0300

    [lab1-8-checkers2] add description

--- labs/checkers2/checkers2/checkers2.cpp
+++ labs/checkers2/checkers2/checkers2.cpp
@@ -1,3 +1,16 @@
+/*
+* 1.8. Шашки 2 (7) 
+* На шахматном поле расположены N черных шашек и одна белая дамка.
+* Требуется написать программу, которая по заданному расположению шашек определяет,
+* какое максимальное количество шашек может взять белая дамка за один ход.
+* 
+* Стандарт C++ 17
+* Среда разработки: Visual Studio 2019
+* ОС: Windows 10
+* 
+* Выполнил: Ермаков Павел, ПС-21
+*/
+
 #include <iostream>
 #include <stack>
 #include <optional>
@@ -5,14 +18,15 @@
 #include <fstream>
 #include <array>
 #include <vector>
-#include <cmath>
 
-constexpr short BLANK = '0';
-constexpr short CHECKER_BLACK = '1';
-constexpr short CHECKER_WHITE = '2';
+constexpr char BLANK = '0';
+constexpr char CHECKER_BLACK = '1';
+constexpr char CHECKER_WHITE = '2';
+constexpr short COORDINATE_SHIFT = 1;
 constexpr size_t MIN_POS = 1;
 constexpr size_t MAX_POS = 8;
-const std::string outputFileName = "OUTPUT.TXT";
+const std::string INPUT_FILE_NAME = "INPUT.TXT";
+const std::string OUTPUT_FILE_NAME = "OUTPUT.TXT";
 
 struct Move
 {
@@ -55,29 +69,16 @@ struct Point
 struct Capture
 {
 	Point whitePos;
-	Point blackPos;
 	Point afterPos;
 
-	Capture(Point whitePos, Point blackPos, Point afterPos)
-		: whitePos(whitePos), blackPos(blackPos), afterPos(afterPos)
+	Capture(Point whitePos, Point afterPos)
+		: whitePos(whitePos), afterPos(afterPos)
 	{
 	}
 };
 
-struct BoardCell
-{
-	char figure = BLANK;
-	bool captured = false;
-};
-
-struct MoveNode {
-	struct Move move;
-	struct MoveNode* firstchild;
-	struct MoveNode* nextsibling;
-};
-
 typedef std::vector<Capture> CaptureVec;
-typedef std::array<std::array<BoardCell, MAX_POS>, MAX_POS> Board;
+typedef std::array<std::array<char, MAX_POS>, MAX_POS> Board;
 
 Point FillBoard(std::ifstream& inputFile, Board& board)
 {
@@ -89,12 +90,12 @@ Point FillBoard(std::ifstream& inputFile, Board& board)
 
 		for (size_t col = 0; col < MAX_POS; ++col)
 		{
-			board[row][col].figure = line[col];
+			board[row][col] = line[col];
 
 			if (line[col] == CHECKER_WHITE)
 			{
 				startPoint = { row, col };
-				board[row][col].figure = BLANK;
+				board[row][col] = BLANK;
 			}
 		}
 	}
@@ -119,12 +120,14 @@ bool InRange(T value, T min, T max)
 {
 	return value >= min && value <= max;
 }
-CaptureVec MakeMove(const Move& move, const Move& moveDirect, Point& startPoint, Board& board, CaptureVec& captureVec);
-CaptureVec MakeCapture(const Move& prevMoveDirect, Point& startPoint, Board board, CaptureVec captureVec)
+
+CaptureVec MakeMove(const Move& move, const Move& moveDirect, const Point& startPoint, Board& board, CaptureVec& captureVec);
+CaptureVec MakeCapture(const Move& prevMoveDirect, const Point& startPoint, Board board, CaptureVec captureVec)
 {
 	CaptureVec interResult;
 	CaptureVec result = captureVec;
-	if (startPoint.x < MAX_POS - 1 && startPoint.y < MAX_POS - 1)
+	if (startPoint.x < MAX_POS - COORDINATE_SHIFT && 
+		startPoint.y < MAX_POS - COORDINATE_SHIFT)
 	{
 		if (prevMoveDirect.dx != -OFFSET_DR.dx || prevMoveDirect.dy != -OFFSET_DR.dy)
 		{
@@ -138,7 +141,8 @@ CaptureVec MakeCapture(const Move& prevMoveDirect, Point& startPoint, Board boar
 			}
 		}
 	}
-	if (startPoint.x > MIN_POS - 1 && startPoint.y > MIN_POS - 1)
+	if (startPoint.x > MIN_POS - COORDINATE_SHIFT && 
+		startPoint.y > MIN_POS - COORDINATE_SHIFT)
 	{
 		if (prevMoveDirect.dx != -OFFSET_UL.dx || prevMoveDirect.dy != -OFFSET_UL.dy)
 		{
@@ -152,7 +156,8 @@ CaptureVec MakeCapture(const Move& prevMoveDirect, Point& startPoint, Board boar
 			}
 		}
 	}
-	if (startPoint.x < MAX_POS - 1 && startPoint.y > MIN_POS - 1)
+	if (startPoint.x < MAX_POS - COORDINATE_SHIFT && 
+		startPoint.y > MIN_POS - COORDINATE_SHIFT)
 	{
 		if (prevMoveDirect.dx != -OFFSET_DL.dx || prevMoveDirect.dy != -OFFSET_DL.dy)
 		{
@@ -166,7 +171,8 @@ CaptureVec MakeCapture(const Move& prevMoveDirect, Point& startPoint, Board boar
 			}
 		}
 	}
-	if (startPoint.x > MIN_POS - 1 && startPoint.y < MAX_POS - 1)
+	if (startPoint.x > MIN_POS - COORDINATE_SHIFT && 
+		startPoint.y < MAX_POS - COORDINATE_SHIFT)
 	{
 		if (prevMoveDirect.dx != -OFFSET_UR.dx || prevMoveDirect.dy != -OFFSET_UR.dy)
 		{
@@ -180,32 +186,26 @@ CaptureVec MakeCapture(const Move& prevMoveDirect, Point& startPoint, Board boar
 			}
 		}
 	}
-	if (result.size() > 0)
-	{
-		return result;
-	}
-	else
-	{
-		return captureVec;
-	}
-	//return result.size() > 0 ? result : captureVec;
+
+	return result;
 }
 
-CaptureVec MakeMove(const Move& move, const Move& moveDirect, Point& startPoint, Board& board, CaptureVec& captureVec)
+CaptureVec MakeMove(const Move& move, const Move& moveDirect, const Point& startPoint, Board& board, CaptureVec& captureVec)
 {
 	Point currPoint = { startPoint.x + move.dx, startPoint.y + move.dy };
-	if (board[currPoint.x][currPoint.y].figure == CHECKER_BLACK && 
-		board[currPoint.x + moveDirect.dx][currPoint.y + moveDirect.dy].figure != CHECKER_BLACK &&
+	if (board[currPoint.x][currPoint.y] == CHECKER_BLACK && 
+		board[currPoint.x + moveDirect.dx][currPoint.y + moveDirect.dy] != CHECKER_BLACK &&
 		InRange(currPoint.x, MIN_POS, MAX_POS - 2) && InRange(currPoint.y, MIN_POS, MAX_POS - 2))
 	{
 		Point afterPoint = { currPoint.x + moveDirect.dx, currPoint.y + moveDirect.dy };
-		CaptureVec interResult;
 		CaptureVec result;
-		Capture capture = { startPoint, currPoint, afterPoint };
+		Capture capture = { startPoint, afterPoint };
 		captureVec.push_back(capture);
-		board[currPoint.x][currPoint.y].figure = BLANK;
-		while (InRange(currPoint.x, MIN_POS - 1, MAX_POS - 1) && InRange(currPoint.y, MIN_POS - 1, MAX_POS - 1) &&
-			board[currPoint.x][currPoint.y].figure != CHECKER_BLACK)
+		board[currPoint.x][currPoint.y] = BLANK;
+
+		while (InRange(currPoint.x, MIN_POS - COORDINATE_SHIFT, MAX_POS - COORDINATE_SHIFT) && 
+			InRange(currPoint.y, MIN_POS - COORDINATE_SHIFT, MAX_POS - COORDINATE_SHIFT) &&
+			board[currPoint.x][currPoint.y] != CHECKER_BLACK)
 		{
 			Point nextPoint = { currPoint.x + moveDirect.dx, currPoint.y + moveDirect.dy };
 			CaptureVec interResult = MakeCapture(moveDirect, nextPoint, board, captureVec);
@@ -214,21 +214,24 @@ CaptureVec MakeMove(const Move& move, const Move& moveDirect, Point& startPoint,
 				result.clear();
 				result.insert(result.begin(), interResult.begin(), interResult.end());
 			}
+
 			currPoint.x += moveDirect.dx;
 			currPoint.y += moveDirect.dy;
 		}
+
 		return result;
 	}
 	else
 	{
-		if (board[currPoint.x + moveDirect.dx][currPoint.y + moveDirect.dy].figure == CHECKER_BLACK &&
-			board[currPoint.x][currPoint.y].figure == CHECKER_BLACK)
+		if (board[currPoint.x][currPoint.y] == CHECKER_BLACK &&
+			board[currPoint.x + moveDirect.dx][currPoint.y + moveDirect.dy] == CHECKER_BLACK )
 		{
 			return captureVec;
 		}
 		else if (InRange(currPoint.x, MIN_POS, MAX_POS - 2) && InRange(currPoint.y, MIN_POS, MAX_POS - 2))
 		{
 			CaptureVec result = MakeMove(move + moveDirect, moveDirect, startPoint, board, captureVec);
+
 			return result;
 		}
 		else
@@ -238,27 +241,51 @@ CaptureVec MakeMove(const Move& move, const Move& moveDirect, Point& startPoint,
 	}
 }
 
-int main(int argc, char* argv[])
+bool PrintCaptureVec(const CaptureVec& resultVec, std::ofstream& outputFile)
 {
-	std::ifstream inputFile(argv[1]);
+	outputFile << resultVec.size() << std::endl;
+
+	for (size_t i = 0; i < resultVec.size(); i++)
+	{
+		outputFile << resultVec.at(i).whitePos.x + COORDINATE_SHIFT 
+			<< " " << resultVec.at(i).whitePos.y + COORDINATE_SHIFT << std::endl;
+		if (i == resultVec.size() - 1)
+		{
+			outputFile << resultVec.at(i).afterPos.x + COORDINATE_SHIFT 
+				<< " " << resultVec.at(i).afterPos.y + COORDINATE_SHIFT << std::endl;
+		}
+	}
+
+	if (!outputFile.flush())
+	{
+		std::cout << "Can't save data on disk\n";
+
+		return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	std::ifstream inputFile(INPUT_FILE_NAME);
 	bool status = ValidateFile(inputFile);
 	if (!status)
 	{
 		return 1;
 	}
+
 	Board board;
 	Point startPoint = FillBoard(inputFile, board);
 	CaptureVec captureVec;
 	CaptureVec resultVec = MakeCapture(NO_MOVE, startPoint, board, captureVec);
-
-	std::cout << resultVec.size() << std::endl;
-	for (size_t i = 0; i < resultVec.size(); i++)
+	
+	std::ofstream outputFile(OUTPUT_FILE_NAME);
+	status = PrintCaptureVec(resultVec, outputFile);
+	if (!status)
 	{
-		std::cout << resultVec.at(i).whitePos.x + 1 << " " << resultVec.at(i).whitePos.y + 1 << std::endl;
-		if (i == resultVec.size() - 1)
-		{
-			std::cout << resultVec.at(i).afterPos.x + 1 << " " << resultVec.at(i).afterPos.y + 1;
-		}
+		return 1;
 	}
+
 	return 0;
 }
