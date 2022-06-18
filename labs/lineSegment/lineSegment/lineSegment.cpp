e55cae24fada230f3b736d2c13d0dd8b7c5d4d43
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-06-19 00:00:03 +0300

    [lab11-8] add LineSegment equation

--- labs/lineSegment/lineSegment/lineSegment.cpp
+++ labs/lineSegment/lineSegment/lineSegment.cpp
@@ -1,20 +1,140 @@
 // lineSegment.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <fstream>
 #include <iostream>
+#include <optional>
+
+const std::string INPUT_FILE_NAME = "INPUT.TXT";
+const std::string OUTPUT_FILE_NAME = "OUTPUT.TXT";
+
+constexpr int NO_POSITION = 0;
+
+struct Point
+{
+	double x;
+	double y;
+
+	friend std::ifstream& operator>>(std::ifstream& is, Point& point)
+	{
+		is >> point.x >> point.y;
+
+		return is;
+	}
+};
+
+struct LineSegment
+{
+	Point A;
+	Point B;
+};
+
+typedef std::optional<double> Position;
+
+bool ValidateFile(const std::ifstream& inputFile);
+bool LineEquation(Point const& A, Point const& B, double x, Point& result);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::ifstream inputFile(INPUT_FILE_NAME);
+	bool status = ValidateFile(inputFile);
+	if (!status)
+	{
+		return 1;
+	}
+
+	Point A;
+	Point B;
+	Point C;
+	Point D;
+
+	inputFile >> A >> B >> C >> D;
+
+	LineSegment firstLineSegment{ A, B };
+	LineSegment secondLineSegment{ C, D };
+
+	Position xIntersection = GetXIntersectionPoint(firstLineSegment, secondLineSegment);
+	
+	if (!xIntersection.has_value())
+	{
+		std::cout << "No" << std::endl;
+
+		return 0;
+	}
+
+	Point result{ xIntersection.value(), NO_POSITION };
+	bool isComputable;
+	if (!isComputable)
+	{
+		std::cout << "No" << std::endl;
+
+		return 0;
+	}
+
+	if (result.y == NAN)
+	{
+		bool isComputable = LineEquation(secondLineSegment.A, secondLineSegment.B, result.x, result);
+		if (!isComputable && result.y == NAN)
+		{
+			std::cout << "No" << std::endl;
+
+			return 0;
+		}
+	}
+
+
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool ValidateFile(const std::ifstream& inputFile)
+{
+	if (!inputFile.is_open())
+	{
+		std::cout << "Failed to open input file for reading\n";
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		return false;
+	}
+
+	return true;
+}
+
+Position GetXIntersectionPoint(LineSegment const& firstLineSegment, LineSegment const& secondLineSegment)
+{
+	int rightSide = firstLineSegment.A.x * (firstLineSegment.B.y - firstLineSegment.A.y) -
+					secondLineSegment.A.x * (secondLineSegment.B.y - secondLineSegment.A.y) +
+					secondLineSegment.A.y * (secondLineSegment.B.x - secondLineSegment.A.x) -
+					firstLineSegment.A.y * (firstLineSegment.B.x - firstLineSegment.A.x);
+	int leftSide = (firstLineSegment.B.y - firstLineSegment.A.y) - (secondLineSegment.B.y - secondLineSegment.A.y);
+
+	if (leftSide == 0)
+	{
+		return std::nullopt;
+	}
+
+	return rightSide / leftSide;
+}
+
+bool LineEquation(Point const& A, Point const& B, double x, Point& result)
+{
+	if (B.x == A.x && B.x == x)
+	{
+		result.y = NAN;
+
+		return true;
+	}
+
+	if (B.x == A.x)
+	{
+		return false;
+	}
+
+	result.y = (x - A.x) * (B.y - A.y) / (B.x - A.x) + A.y;
+
+	return true;
+}
+
+void CalculateLineSegmentsIntersection(LineSegment const& firstLineSegment, LineSegment const& secondLineSegment)
+{
+	
+}
